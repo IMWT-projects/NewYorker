48315350081aecc5372bb5a857cd506e34e11445
Lucrezia <ateossg5@gmail.com>
2024-03-05 12:04:21 +0100

    java

--- Javascript/js_exercise.js
+++ Javascript/js_exercise.js
@@ -7,9 +7,9 @@ function checkDate() {
     var date1 = document.getElementById('date1').value;
     var date2 = document.getElementById('date2').value;
     if (date1==''||date2=='') {
-        alert('Attention! Please enter two different dates.');
+        alert('Attention! None of the dates can be empty. Please enter the dates.');
     }   else if (date1==date2) {
-            alert('Attention! The two dates must be different.');
+            alert('Attention! The two dates can\'t be the same. Please change them.');
     }   else {
             alert('Correct!')
     }
